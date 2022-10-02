e261b2be391d0159a4d7a77e75acac43a2c0f130
realovka <realovka@mail.ru>
2022-10-02 13:11:04 +0300

    create dev

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello from master general repo");
+        System.out.println("hello from dev general repo");
+        System.out.println("second stash");
+        System.out.println("third stash");
     }
 }
