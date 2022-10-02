1e5ee224cbf2dd9fe1f8e1a4fa6457527be8a615
realovka <realovka@mail.ru>
2022-10-02 14:47:30 +0300

    create ticket-3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello from ticket-2 general repo");
-        System.out.println("second stash");
-        System.out.println("third stash");
+        System.out.println("hello from ticket-3 general repo");
     }
 }
