98b0e6dbd7ecc8c5cf2f6cfc8f0023436d5b6ef9
realovka <realovka@mail.ru>
2022-10-02 15:22:15 +0300

    create ticket-7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello from ticket-4  general repo");
+        System.out.println("hello from ticket-7  general repo");
     }
 }
